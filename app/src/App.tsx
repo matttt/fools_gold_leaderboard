12ab89c6b71bc536104f802122bbaba8c8d05b19
Matthew Martori <matthew.martori@gmail.com>
2025-07-20 16:54:51 -0700

    init

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-4xl font-bold'>Fool's Gold Leader Board</div>
+    </>
+  )
+}
+
+export default App
---
+++ scraping_aggregator/videoUrls.ts
@@ -0,0 +1,65 @@
+enum VideoName {
+    PeelRobalino,
+    AnnasKingForADay,
+    KatiesD20OnABus,
+    ErikasHaircut,
+    SephiesSexyCarWash,
+    GrantsCrack,
+    JonnysHumanPuppyBowl,
+    LilyAndIzzysMilkTasteTest,
+    IzzysButtholes,
+    VicsBrennansExitVideo
+}
+
+const videoUrls = {
+    [VideoName.PeelRobalino]: {
+        youtube: 'https://www.youtube.com/shorts/gMpx4A2lRTE', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527082801120677133',
+        reels: 'https://www.instagram.com/reel/DMG0jk2tbeR/',
+    },
+    [VideoName.AnnasKingForADay]: {
+        youtube: 'https://www.youtube.com/shorts/UjHk90dxX20', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527078952171523341',
+        reels: 'https://www.instagram.com/reel/DMGy8RuNDqI/',
+    },
+    [VideoName.KatiesD20OnABus]: {
+        youtube: 'https://www.youtube.com/shorts/5feqZBLXrMg', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527080453610704183',
+        reels: 'https://www.instagram.com/reel/DMGzkMNNMXg/',
+    },
+    [VideoName.ErikasHaircut]: {
+        youtube: 'https://www.youtube.com/shorts/wQVIfuNIc9I', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527086113614318862',
+        reels: 'https://www.instagram.com/reel/DMG2ELMvZdg/',
+    },
+    [VideoName.SephiesSexyCarWash]: {
+        youtube: 'https://www.youtube.com/shorts/HD5pyGbO_Is', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527082014449716494',
+        reels: 'https://www.instagram.com/reel/DMG0OcPpSQO/',
+    },
+    [VideoName.GrantsCrack]: {
+        youtube: 'https://www.youtube.com/shorts/1lnl0jYln8s', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527083827689229582',
+        reels: 'https://www.instagram.com/reel/DMG1BIPM41Z/',
+    },
+    [VideoName.JonnysHumanPuppyBowl]: {
+        youtube: 'https://www.youtube.com/shorts/aagwlycxv_k', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527084871580142861',
+        reels: 'https://www.instagram.com/reel/DMG1d62Mhmf/',
+    },
+    [VideoName.LilyAndIzzysMilkTasteTest]: {
+        youtube: 'https://www.youtube.com/shorts/nfwmaVlp_hY', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527085610322890039',
+        reels: 'https://www.instagram.com/reel/DMG11avO8qa/',
+    },
+    [VideoName.IzzysButtholes]: {
+        youtube: 'https://www.youtube.com/shorts/Wm8SMsmWCts', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527086642415422734',
+        reels: 'https://www.instagram.com/reel/DMG2SXRMojo/',
+    },
+    [VideoName.VicsBrennansExitVideo]: {
+        youtube: 'https://www.youtube.com/shorts/oO4kgmYivoQ', 
+        tiktok: 'https://www.tiktok.com/@gamechangershow/video/7527087942339267895',
+        reels: 'https://www.instagram.com/reel/DMG24Zjyg1j/',
+    }
+}
